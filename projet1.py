5ae1cf534e43f008ef47d2e07317b8022461d0be
melanieferrier <81634652+melanieferrier@users.noreply.github.com>
2021-11-24 11:20:11 +0100

    Update projet1.py

--- projet1.py
+++ projet1.py
@@ -8,14 +8,12 @@ import cvxpy as cp
 
 #2.
 
-a=1
-b=1
 L=2
 
 def c(z):
     n=np.size(z)[0]/2
-    x=[0]+z[0:n,:]+[a]
-    y=[0]+z[n:,:]+[b]
+    x=[0]+z[0:n,:]
+    y=[0]+z[n:,:]
     c=np.zeros((n+1,1))
     for i in range (n+1):
         l=(x[i+1]-x[i])**2+(y[i+1]-y[i])**2
@@ -39,4 +37,19 @@ def lag(z,lambda):
     for i in range (n):
         e[i+n,0]=1
     return np.transpose(e)*z + np.transpose(lambda)*c(z)
-    
+
+
+#5.
+def dzlag(z,lambda):
+    n=np.size(z)[0]/2
+    e=np.zeros((2*n,1))
+    for i in range (n):
+        e[i+n,0]=1
+    x=[0]+z[:n,:]
+    y=[0]+z[n:,:]
+    dzc=np.zeros((2*n,n+1))
+    for i in range(n):
+        for j in range(n+1):
+            dzc[i,j]=2*(x[i+1]-x[i])
+            dzc[i+n,j]=2*(y[i+1]-y[i])
+    return e + np.transpose(dzc)*lambda
